1e9eeb607f56585f0b9ac4b65d8964ec78de883d
bangawhoBack <beogyahyeo@gamil.com>
2024-11-19 17:06:15 +0900

    2024-11-07

---
+++ src/main/java/com/example/firstproject1/FirstporjeckApplication.java
@@ -0,0 +1,11 @@
+package com.example.firstproject1;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstporjeckApplication {
+    public static void main(String[] args) { SpringApplication.run(FirstporjeckApplication.class, args);
+
+
+    }
+}
--- src/main/java/com/example/firstproject1/controller/ArticleController.java
+++ src/main/java/com/example/firstproject1/controller/ArticleController.java
@@ -1,26 +1,32 @@
 package com.example.firstproject1.controller;
+import ch.qos.logback.classic.Logger;
 import com.example.firstproject1.entity.Article;
 import com.example.firstproject1.dto.ArticleForm;
 import com.example.firstproject1.repository.ArticleRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class ArticleController {
 
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(ArticleController.class);
     @Autowired // 스프링부트가 미리 생성해놓은 객체를 가져다가 자동으로 연결!
     private ArticleRepository articleRepository;
 
-    @GetMapping("/article/new")
+    @GetMapping("/articles/new")
     public String newArticle() {
-        return "article/new";
+        return "articles/new";
     }
 
-    @PostMapping("/article/create")
+    @PostMapping("/articles/create")
     public String creteArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
 
         // 1. Dto 객체를 변환 -> Entity !!
         Article article = form.toEntity();
@@ -31,4 +37,22 @@ public class ArticleController {
         System.out.println(saved.toString());
         return "";
     }
+
+
+
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+
+        // 1. id로 데이터를 가져옴.
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 가져온 데이터를 모델에 등록함.
+        model.addAttribute("articles", articleEntity);
+
+        // 3. 보여줄 페이지를 결정!
+        return "articles/show";
+    }
 }
+
